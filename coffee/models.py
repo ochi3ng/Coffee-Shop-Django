07c22080df8479239ce8d94a442e82d93e54f6d8
ochi3ng <johnochi3ng@gmail.com>
2024-11-22 14:37:26 +0300

    Adding login and logout function

--- coffee/models.py
+++ coffee/models.py
@@ -22,15 +22,17 @@ class Customer(models.Model):
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
     
-    # Products
+    #All of our Products
 class Product(models.Model):
     name =models.CharField(max_length=100)
     price =models.DecimalField(default=0, decimal_places=2,max_digits=7)
     category =models.ForeignKey(category, on_delete=models.CASCADE, default=1)
     description =models.CharField(max_length=250, default='', blank=True, null=True)
     image =models.ImageField(upload_to='uploads/product')
-
-    def __str__(self):
+  #Add Sale Stuff
+is_sale = models.BooleanField(default=False)
+sale_price =models.DecimalField(default=0, decimal_places=2,max_digits=7)
+def __str__(self):
         return self.name
     
     #customer order
--- coffee/views.py
+++ coffee/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from django.http import HttpResponse
 from . models import Coffee, Product
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
 
 
 
@@ -8,3 +11,29 @@ def home(request):
     products = Product.objects.all()
     coffee = Coffee.objects.all()
     return render(request, 'home.html', {'products':products})
+
+
+def about (request):
+    return render(request, 'about.html', {})
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        if not username or not password:
+            messages.error(request, "Both username and password are required.")
+            return render(request, "login.html")
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("home")
+        else:
+            messages.error(request, "Invalid username or password.")
+    return render(request, "login.html")
+
+def logout_user (request):
+    logout(request)
+    messages.success(request, ('You have been logged out...'))
+    return redirect('login')
